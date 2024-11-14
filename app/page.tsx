5ca65142d39cf5b3d62e134b35dd241b60e0f76e
Victor Lebecq <victor.lebecq.pro@gmail.com>
2024-11-14 19:32:06 +0100

    Made a first version of the landing page header

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import { buttonVariants } from '@/src/components/ui/button';
-import Link from 'next/link';
+import { Header } from '@/src/components/header';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 py-20">
-      <Link href={'/login'} className={buttonVariants({ variant: 'default' })}>
-        Login
-      </Link>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import { buttonVariants } from './ui/button';
+import { ArrowRight } from 'lucide-react';
+
+export function Header() {
+  return (
+    <header className="flex min-h-[60px] items-center justify-between lg:flex lg:px-4 xl:px-0">
+      <div className="container mx-auto flex w-full items-center justify-between rounded-2xl px-[3px] backdrop-blur-xl">
+        <div className="flex flex-1 items-center gap-2 pl-3">
+          <Link href={'/'} className="flex items-center gap-1.5">
+            LOGO
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-end gap-2 pl-3">
+          <Link href={'/login'} className={buttonVariants({ variant: 'secondary' })}>
+            Sign in
+          </Link>
+          <Link href={'/login'} className={buttonVariants({ variant: 'default' })}>
+            Try name for free <ArrowRight />
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
