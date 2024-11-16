f4356c9e5df74454eeb63694804ec065dc512a67
Victor Lebecq <victor.lebecq.pro@gmail.com>
2024-11-16 19:26:27 +0100

    Login and logout is working properly now

--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -5,30 +5,18 @@ import { buttonVariants } from './ui/button';
 
 export function LogoutButton() {
   const handleLogout = async () => {
-    try {
-      await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/sanctum/csrf-cookie`, {
+    await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/sanctum/csrf-cookie`, {
+      withCredentials: true,
+    });
+
+    await axios.post(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/logout`,
+      {},
+      {
         withCredentials: true,
-      });
-
-      const response = await axios.post(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/logout`,
-        {},
-        {
-          withCredentials: true,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (response.status === 200) {
-        console.log('Logged out successfully');
-      } else {
-        throw new Error('Failed to log out');
+        withXSRFToken: true,
       }
-    } catch (error) {
-      console.error('Error during logout:', error);
-    }
+    );
   };
 
   return (
